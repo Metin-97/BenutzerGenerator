94e925bb94b321129c304dc43d7c6d5397a1e174
Metin-97 <79480448+Metin-97@users.noreply.github.com>
2021-06-18 16:16:04 +0200

    Add files via upload

---
+++ ClsBerechtigung.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenutzerGenerator_MetinDereli.Funktionen
+{
+   public class ClsBerechtigung : FrmBenutzerGenerator
+    {
+        public void _uGRPBerechtigungee()
+        {
+            //Berechtigungen von den RadioButton auf die Parameter Initialisieren
+            if (rbBerechtigung1.Checked == true)
+            {
+                parInUGRPId = 7;
+            }
+            //--
+            if (rbBerechtigung2.Checked == true)
+            {
+                parInUGRPId = 12;
+            }
+            //--
+            if (rbBerechtigung3.Checked == true)
+            {
+                parInUGRPId = 15;
+            }
+            //--
+            if (rbBerechtigung4.Checked == true)
+            {
+                parInUGRPId = 17;
+            }
+            //--
+            if (rbBerechtigung5.Checked == true)
+            {
+                parInUGRPId = 18;
+            }
+            //--
+        }
+    }
+}
---
+++ ClsSlideDrawFunc.cs
@@ -0,0 +1,71 @@
+﻿using BenutzerGenerator_MetinDereli.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenutzerGenerator_MetinDereli.Funktionen
+{
+   public class ClsSlideDrawFunc : FrmBenutzerGenerator
+    {
+        public void _drawerSlideMenu()
+        {
+            //Drawer Menu Öffnen
+            //--
+            if (HidenSlideMenu)
+            {
+                pnlWidthDrawer = 250;
+                pnlSlideMenu.Width = pnlSlideMenu.Width + 10;
+                if (pnlSlideMenu.Width >= pnlWidthDrawer)
+                {
+                    timeMenu.Stop();
+                    HidenSlideMenu = false;
+                }
+            }
+            //--
+
+            //Drawer Menu 
+            else
+            {
+                pnlSlideMenu.Width = pnlSlideMenu.Width - 10;
+                if (pnlSlideMenu.Width <= 0)
+                {
+                    timeMenu.Stop();
+                    HidenSlideMenu = true;
+                }
+            }
+            //--
+        }
+        //--
+
+        //DropDown Menu wird erstellt
+        public void _dropDownMenu()
+        {
+            //DropDown Menu 
+            if (HidenSlideDropDown)
+            {
+                btnDropBerechtigungen.Image = Resources.icons8_collapse_arrow_26px;
+                pnlHeightDropDown = 211;
+                pnlDropdown.Height = pnlDropdown.Height + 50;
+                if (pnlDropdown.Height >= pnlHeightDropDown)
+                {
+                    time2Dropdown.Stop();
+                    HidenSlideDropDown = false;
+                }
+            }
+            else
+            {
+                btnDropBerechtigungen.Image = Resources.icons8_expand_arrow_26px;
+                pnlDropdown.Height = pnlDropdown.Height - 50;
+                if (pnlDropdown.Height <= 47)
+                {
+                    time2Dropdown.Stop();
+                    HidenSlideDropDown = true;
+                }
+            }
+            //--
+        }
+        //--
+    }
+}
